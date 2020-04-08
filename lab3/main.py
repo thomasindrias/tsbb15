067e89c7b04e5ba285401def80d239bbded5cefe
johanforslund <johanforslund95@gmail.com>
2020-04-08 15:54:11 +0200

    Refactored RANSAC

--- lab3/main.py
+++ lab3/main.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 import random
+from ransac import ransac
 
 plt.set_cmap("gray")
 
@@ -24,42 +25,9 @@ plt.set_cmap("gray")
 
 (corr1, corr2) = get_corr(im1, im2)
 
-best_inlier_count = 0
-best_F = None
-best_std = np.inf
-
 print("Total points: ", corr1.shape[1])
 
-for i in range(2000):
-    random_index = random.sample(range(corr1.shape[1]), 8)
-
-    corr1_rand = corr1[:, random_index]
-    corr2_rand = corr2[:, random_index]
-
-    F = lab3.fmatrix_stls(corr1_rand, corr2_rand)
-
-    d = lab3.fmatrix_residuals(F, corr1, corr2)
-
-    thresh = 1
-    inlier_count = 0
-
-    for j in range(d.shape[1]):
-        norm = np.linalg.norm(d[:, j])
-        if norm < thresh:
-            inlier_count = inlier_count + 1
-    
-    std = np.std(np.linalg.norm(d, axis=0))
-    
-    if (inlier_count > best_inlier_count) or (inlier_count == best_inlier_count and std < best_std):
-        best_std = std
-        best_inlier_count = inlier_count
-        best_F = F
-        best_corr1 = corr1_rand
-        best_corr2 = corr2_rand
-
-inlier_ratio = best_inlier_count / d.shape[1]
-
-print("INLIER RATIO", inlier_ratio)
+(best_corr1, best_corr2, best_F) = ransac(corr1, corr2, 2000)
 
 lab3.show_corresp(im1, im2, corr1, corr2)
 plt.show()
--- lab3/ransac.py
+++ lab3/ransac.py
@@ -12,7 +12,7 @@ def ransac(corr1, corr2, N):
     best_std = np.inf
 
     for i in range(N):
-        random_index = random.sample(range(len(corr1[1])), 8)
+        random_index = random.sample(range(corr1.shape[1]), 8)
 
         corr1_rand = corr1[:, random_index]
         corr2_rand = corr2[:, random_index]
@@ -24,13 +24,13 @@ def ransac(corr1, corr2, N):
         thresh = 1
         inlier_count = 0
 
-        for j in range(len(d)):
+        for j in range(d.shape[1]):
             norm = np.linalg.norm(d[:, j])
             if norm < thresh:
                 inlier_count = inlier_count + 1
-
+        
         std = np.std(np.linalg.norm(d, axis=0))
-
+        
         if (inlier_count > best_inlier_count) or (inlier_count == best_inlier_count and std < best_std):
             best_std = std
             best_inlier_count = inlier_count
@@ -38,7 +38,7 @@ def ransac(corr1, corr2, N):
             best_corr1 = corr1_rand
             best_corr2 = corr2_rand
 
-    inlier_ratio = best_inlier_count / 8
+    inlier_ratio = best_inlier_count / d.shape[1]
 
     print("INLIER RATIO", inlier_ratio)
 
