b57871fcb2c107291f8fde6678667d4add6433e1
johanforslund <johanforslund95@gmail.com>
2020-04-22 20:06:10 +0200

    Fixed some bugs when determining diffusion tensor

--- lab4/estimate_D.py
+++ lab4/estimate_D.py
@@ -10,15 +10,16 @@ def estimate_D(T, m):
     lambda_2 = Tr / 2 - np.sqrt(np.power(Tr, 2) / 4 - Det)
 
     e1 = np.array([lambda_1 - T[:, :, 1, 1], T[:, :, 1, 0]])[:, None, :, :]
+    e1 = e1 / np.linalg.norm(e1, axis=0)
+    
     e2 = np.array([lambda_2 - T[:, :, 1, 1], T[:, :, 1, 0]])[:, None, :, :]
+    e2 = e2 / np.linalg.norm(e2, axis=0)
 
     alpha_1 = np.exp(-lambda_1 / m)
     alpha_2 = np.exp(-lambda_2 / m)
 
-    D = alpha_1 * e1 @ np.transpose(e1, (1, 0, 2, 3)) + alpha_2 * e2 @ np.transpose(
+    D = alpha_1 * e1 * np.transpose(e1, (1, 0, 2, 3)) + alpha_2 * e2 * np.transpose(
         e2, (1, 0, 2, 3)
     )
-
-    print(np.transpose(D, (2, 3, 0, 1)).shape)
 
     return np.transpose(D, (2, 3, 0, 1))
--- lab4/main.py
+++ lab4/main.py
@@ -3,6 +3,8 @@ from estimate_T import estimate_T
 from estimate_D import estimate_D
 from regularized_values import regularized_values
 from matplotlib import pyplot as plt
+import scipy.io as sio
+import numpy as np
 
 ksize = 15
 sigma = 2
@@ -12,7 +14,8 @@ gradsigma = 2
 I = lab4.get_cameraman()
 
 T = estimate_T(I, gradksize, gradsigma, ksize, sigma)
-estimate_D(T, -1)
+sio.savemat('T.mat', {'data': T})
 
-plt.imshow(T[:, :, 0, 0], cmap="gray")
-plt.show()
+D = estimate_D(T, 0.1)
+#plt.imshow(T[:, :, 0, 0], cmap="gray")
+#plt.show()
