7a758a54d0c6519edf5470449dc42a8a41f0d140
thomasindrias <thoin216@student.liu.se>
2020-04-22 13:19:57 +0200

    Calculate D

---
+++ lab4/estimate_D.py
@@ -0,0 +1,24 @@
+import scipy.linalg as la
+import numpy as np
+
+
+def estimate_D(T, m):
+    Tr = T[:, :, 0, 0] + T[:, :, 1, 1]
+    Det = (T[:, :, 1, 1] * T[:, :, 0, 0]) - (T[:, :, 1, 0] * T[:, :, 0, 1])
+
+    lambda_1 = Tr / 2 + np.sqrt(np.power(Tr, 2) / 4 - Det)
+    lambda_2 = Tr / 2 - np.sqrt(np.power(Tr, 2) / 4 - Det)
+
+    e1 = np.array([lambda_1 - T[:, :, 1, 1], T[:, :, 1, 0]])[:, None, :, :]
+    e2 = np.array([lambda_2 - T[:, :, 1, 1], T[:, :, 1, 0]])[:, None, :, :]
+
+    alpha_1 = np.exp(-lambda_1 / m)
+    alpha_2 = np.exp(-lambda_2 / m)
+
+    D = alpha_1 * e1 @ np.transpose(e1, (1, 0, 2, 3)) + alpha_2 * e2 @ np.transpose(
+        e2, (1, 0, 2, 3)
+    )
+
+    print(np.transpose(D, (2, 3, 0, 1)).shape)
+
+    return np.transpose(D, (2, 3, 0, 1))
